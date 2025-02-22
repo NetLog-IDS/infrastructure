466cd907fcea903252a8cb23c2cdad8d9dec2d97
Emyr298 <emi.ru298@gmail.com>
2025-02-22 15:06:27 +0700

    fix: port

--- terraform/scripts/netlog.sh
+++ terraform/scripts/netlog.sh
@@ -5,4 +5,4 @@ sudo docker run --name netlog \
     --network host \
     --entrypoint bash \
     -d \
-    recedivies09/spoofy:latest -c "/wait-for-it.sh $KAFKA_IP:19092 -- /usr/local/bin/spoofy -i ens3 --live --sender kafka --broker $KAFKA_IP:19092 --topic network-traffic"
+    recedivies09/spoofy:latest -c "/wait-for-it.sh $KAFKA_IP:19092 -- /usr/local/bin/spoofy -i eth0 --live --sender kafka --broker $KAFKA_IP:19092 --topic network-traffic"
