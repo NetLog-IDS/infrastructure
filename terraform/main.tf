f428767f2d45c570e7396633d612264c26a18d14
Emyr298 <emi.ru298@gmail.com>
2025-04-12 21:47:48 +0700

    feat: files and refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,17 +7,52 @@ terraform {
   }
 }
 
+variable "configuration" {
+  description = "EC2 configuration"
+  type = list(object({
+    application_name = string
+    ami              = string
+    no_of_instances  = number
+    instance_type    = string
+    script_path      = string
+    params           = list(string)
+    healthcheck_cmd  = string
+  }))
+  default = []
+}
+
+variable "copy_files" {
+  description = "Files to be copied from local to remote"
+  type = list(object({
+    local_path          = string
+    remote_path         = string
+    instance_name_regex = string
+  }))
+  default = []
+}
+
+variable "public_key" {
+  description = "Public key information"
+  type = object({
+    name = string
+    key  = string
+  })
+}
+
+variable "private_key_location" {
+  description = "Private key location"
+  type        = string
+}
+
 provider "aws" {
   region  = "us-east-1"
   profile = "default"
 }
 
-
 resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCulyTEu3/ByEQcnHoSFmO/EddIMXEOsJ+jEHp4lL1Bz1tfTIysiQpqnI8Jj+2L1nAthMx9Cq5aw8feQtTsxe6Ipjqifzk/K3raPIBRcZpwTu0FvhpgeK4hNQPfKemfo5aavz8F79cN2+BbcLf1gVf9pazmyEV4Vqi/enHsYvZkxW4rUBkodXvSBmYeYMnJ2ALt9m2mACB4Af/2YcGuYIqCoyRwydYEHVMnmBZkCwPDt2/VaUVkGfBYTAFIFZdKByN81OEN8nNzkSRjcxQtAuJ3hFBJWQCevo8ftr0pjBrEoLuuiIqescCCdw71FD8fZkUuzhn5XlbqQdVnA6+zhC7H ta@jomamas.csl"
+  key_name   = var.public_key.name
+  public_key = var.public_key.key
 }
-
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
@@ -38,43 +73,20 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-variable "configuration" {
-  description = "EC2 configuration"
-  default     = [{}]
-}
-
 locals {
-  serverconfig = [
+  instances = flatten([
     for srv in var.configuration : [
       for i in range(1, srv.no_of_instances + 1) : {
-        instance_name = "${srv.application_name}-${i}"
-        instance_type = srv.instance_type
-        ami           = srv.ami
-        script_path   = srv.script_path
-        params        = srv.params
+        instance_name   = "${srv.application_name}-${i}"
+        instance_type   = srv.instance_type
+        ami             = srv.ami
+        script_path     = srv.script_path
+        params          = srv.params
         healthcheck_cmd = srv.healthcheck_cmd
       }
     ]
-  ]
+  ])
 }
-
-locals {
-  instances = flatten(local.serverconfig)
-}
-
-/*
-resource "aws_instance" "my_vm_1" {
-  ami                         = "ami-0e1bed4f06a3b463d"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  key_name                    = "tugasakhir"
-  security_groups             = [aws_security_group.allow_ssh.name]
-
-  tags = {
-    Name = "my_vm_1"
-  }
-}
-*/
 
 resource "aws_instance" "example_server" {
   for_each = { for server in local.instances : server.instance_name => server }
@@ -82,10 +94,9 @@ resource "aws_instance" "example_server" {
   ami                         = each.value.ami
   instance_type               = each.value.instance_type
   associate_public_ip_address = true
-  # key_name                    = aws_key_pair.deployer.key_name
-  key_name        = "tugasakhir"
-  security_groups = [aws_security_group.allow_ssh.name]
-  user_data       = <<-EOF
+  key_name                    = aws_key_pair.deployer.key_name
+  security_groups             = [aws_security_group.allow_ssh.name]
+  user_data                   = <<-EOF
               #!/bin/bash
               sudo apt update && sudo apt upgrade -y
               curl -fsSl https://get.docker.com -o get-docker.sh
@@ -113,8 +124,7 @@ resource "terraform_data" "container_setup" {
       type        = "ssh"
       user        = "ubuntu"
       host        = aws_instance.example_server[each.value.instance_name].public_ip
-      # private_key = file("~/.ssh/deployer-key")
-      private_key = file("./keys/tugasakhir.pem")
+      private_key = file(var.private_key_location)
     }
   }
 
@@ -134,8 +144,36 @@ resource "terraform_data" "container_setup" {
       type        = "ssh"
       user        = "ubuntu"
       host        = aws_instance.example_server[each.value.instance_name].public_ip
-      # private_key = file("~/.ssh/deployer-key")
-      private_key = file("./keys/tugasakhir.pem")
+      private_key = file(var.private_key_location)
+    }
+  }
+}
+
+resource "terraform_data" "transfer_files" {
+  for_each = {
+    for pairs in flatten([
+      for file in var.copy_files : [
+        for server in local.instances : {
+          local_path    = file.local_path
+          remote_path   = file.remote_path
+          instance_name = server.instance_name
+        }
+        if can(regex(file.instance_name_regex, server.instance_name))
+      ]
+    ]) : "${pairs.instance_name}-${pairs.local_path}" => pairs
+  }
+
+  depends_on = [aws_instance.example_server]
+
+  provisioner "file" {
+    source      = each.value.local_path
+    destination = each.value.remote_path
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.example_server[each.value.instance_name].public_ip
+      private_key = file(var.private_key_location)
     }
   }
 }
