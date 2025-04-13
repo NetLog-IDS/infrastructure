ed81239e34389e1e466bda1d18018c4dbd2f2a57
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2025-04-13 17:55:01 +0700

    Scripts for monitoring

--- terraform/scripts/monitoring.sh
+++ terraform/scripts/monitoring.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+KAFKA_IP=$1
+
+sudo docker network create monitoring-network
+
+docker run -d \
+  --name mongodb \
+  --network monitoring-network \
+  -e MONGO_INITDB_ROOT_USERNAME=mongoadmin \
+  -e MONGO_INITDB_ROOT_PASSWORD=secret \
+  mongo
+
 sudo docker run -d \
     --name monitoring \
     --hostname monitoring \
     -p 8000:8000 \
-    -e KAFKA_BROKER="localhost:9092" \
-    -e CONNECTION_STRING="mongodb://mongoadmin:secret@localhost:27017" \
+    -e KAFKA_BROKER=$KAFKA_IP \
+    -e CONNECTION_STRING="mongodb://mongoadmin:secret@mongodb:27017" \
     -e MAIL_USERNAME="to be added" \
     -e MAIL_PASSWORD="to be added (google email token)" \
     -e MAIL_FROM="to be added" \
