2435b1350c5ee7ae292a66647f77abd11a108eb8
recedivies <ahmadhiprananta@gmail.com>
2025-04-14 10:44:28 +0700

    fix: manual scripts netlog

---
+++ terraform/manual-scripts/netlog.sh
@@ -0,0 +1,23 @@
+sudo apt-get install python3-pip
+pip3 install gdown # /home/ubuntu/.local/bin/gdown
+
+# exit then ssh again
+cd /tmp
+/home/ubuntu/.local/bin/gdown --id 1ywI9r1UsyFpGVV_aHzLk-Z3OL-UnFBja
+/home/ubuntu/.local/bin/gdown --id 1MXcaagodK8v8MfGkIlj3Iiz8yCONh06L
+
+# change /tmp/friday_test.pcap to file you want to use
+sudo docker run \
+  --rm \
+  --name netlog \
+  --hostname netlog \
+  -v /tmp/friday_test.pcap:/test.pcap \
+  --network host \
+  --entrypoint bash \
+  recedivies09/spoofy:latest \
+  -c "/usr/local/bin/spoofy \
+      -i /test.pcap \
+      -f 'tcp or udp' \
+      --sender kafka \
+      --broker 13.217.149.8:19092 \
+      --topic network-traffic"
