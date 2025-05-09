a95c71e89196a3cfbcf750d52b72264dbcac4390
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2025-05-09 16:09:59 +0700

    Update scrip.

--- terraform/manual-scripts/netlog.sh
+++ terraform/manual-scripts/netlog.sh
@@ -3,8 +3,9 @@ pip3 install gdown # /home/ubuntu/.local/bin/gdown
 
 # exit then ssh again
 cd /tmp
-/home/ubuntu/.local/bin/gdown --id 1ywI9r1UsyFpGVV_aHzLk-Z3OL-UnFBja
-/home/ubuntu/.local/bin/gdown --id 1MXcaagodK8v8MfGkIlj3Iiz8yCONh06L
+# /home/ubuntu/.local/bin/gdown --id 1ywI9r1UsyFpGVV_aHzLk-Z3OL-UnFBja
+# /home/ubuntu/.local/bin/gdown --id 1MXcaagodK8v8MfGkIlj3Iiz8yCONh06L
+/home/ubuntu/.local/bin/gdown --id 1eDadFhQntu-3ED2DKtCwP4mnSuaB_zBT
 
 # change /tmp/friday_test.pcap to file you want to use
 # Change broker IP on --broker to Kafka virtual machine's IP
@@ -12,7 +13,7 @@ sudo docker run \
   --rm \
   --name netlog \
   --hostname netlog \
-  -v /tmp/friday_test.pcap:/test.pcap \
+  -v /tmp/test.pcap:/test.pcap \
   --network host \
   --entrypoint bash \
   recedivies09/spoofy:latest \
@@ -20,6 +21,21 @@ sudo docker run \
       -i /test.pcap \
       -f 'tcp or udp' \
       --sender kafka \
+      --broker 13.217.59.217:19092 \
+      --topic network-traffic"
+
+# Manual
+sudo docker run \
+  --rm \
+  --name netlog \
+  --hostname netlog \
+  --network host \
+  --entrypoint bash \
+  recedivies09/spoofy:latest \
+  -c "/usr/local/bin/spoofy \
+      -i <Network_Inteface> \
+      --live \
+      -f 'tcp or udp' \
+      --sender kafka \
       --broker 13.217.149.8:19092 \
-      --topic network-traffic \
-      --replay"
+      --topic network-traffic"
--- terraform/manual-scripts/netlogdivies.sh
+++ terraform/manual-scripts/netlogdivies.sh
@@ -3,8 +3,9 @@ pip3 install gdown # /home/ubuntu/.local/bin/gdown
 
 # exit then ssh again
 cd /tmp
-/home/ubuntu/.local/bin/gdown --id 1ywI9r1UsyFpGVV_aHzLk-Z3OL-UnFBja
-/home/ubuntu/.local/bin/gdown --id 1MXcaagodK8v8MfGkIlj3Iiz8yCONh06L
+# /home/ubuntu/.local/bin/gdown --id 1ywI9r1UsyFpGVV_aHzLk-Z3OL-UnFBja
+# /home/ubuntu/.local/bin/gdown --id 1MXcaagodK8v8MfGkIlj3Iiz8yCONh06L
+/home/ubuntu/.local/bin/gdown --id 1eDadFhQntu-3ED2DKtCwP4mnSuaB_zBT
 
 # change /tmp/friday_test.pcap to file you want to use
 # Change broker IP on --broker to Kafka virtual machine's IP
@@ -16,6 +17,6 @@ sudo docker run --name netlogdivies \
     -i /test.pcap \
     -f "tcp or udp" \
     --sender kafka \
-    --broker 3.92.3.91:19092 \
+    --broker 13.218.213.204:19092 \
     --topic network-traffic \
     --mode ordered
