01cacaaba91b166ed9bac7bb516eb409c412404b
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2025-05-12 16:31:31 +0700

    Fix netlog-old and netlog-new scripts

--- terraform/scripts/netlog-new.sh
+++ terraform/scripts/netlog-new.sh
@@ -3,18 +3,4 @@ KAFKA_IP=$1
 
 sudo docker pull recedivies09/netlog-new
 
-sudo apt-get install python3-pip
-pip3 install gdown # /home/ubuntu/.local/bin/gdown
-
-# exit then ssh again
-cd /tmp
-
-# DOS Only
-/home/ubuntu/.local/bin/gdown 1o264oRpwkKA71hnpGbKQH37jRcb30AD5
-
-# PortScan
-/home/ubuntu/.local/bin/gdown 1DPJvuuiXvxBt1AX9P55BThDkJ9ISk5sv
-
-sudo apt install tcpreplay
-
 while ! nc -z $KAFKA_IP 19092; do sleep 5; echo 'Waiting for Kafka...'; done
